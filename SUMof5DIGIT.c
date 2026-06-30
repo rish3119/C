0db93913df866504019231f0fe2b504f79e74ae2
Rishav kumar <65082283+rish3119@users.noreply.github.com>
2026-06-30 22:51:14 +0530

    Add files via upload

---
+++ SUMof5DIGIT.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int sum = 0;
+
+    while (n > 0) {
+        sum += n % 10;   // extract last digit
+        n /= 10;         // remove last digit
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
